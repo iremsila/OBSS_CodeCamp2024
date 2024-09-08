d6bd05b115d7e7c658e6ae35297aed689442cf66
İrem Sıla Yıldıım <83502239+iremsila@users.noreply.github.com>
2024-09-08 13:42:44 +0300

    Update ApiService.kt

--- app/src/main/java/com/iremsilayildirim/homework1/network/service/ApiService.kt
+++ app/src/main/java/com/iremsilayildirim/homework1/network/service/ApiService.kt
@@ -6,45 +6,45 @@ import com.iremsilayildirim.homework1.network.model.MovieResponse
 import com.iremsilayildirim.homework1.network.model.Movies
 import retrofit2.Response
 import retrofit2.http.GET
-import retrofit2.http.Headers
+import retrofit2.http.Header
 import retrofit2.http.Path
 import retrofit2.http.Query
 
 interface ApiService {
 
+    companion object {
+        private const val API_KEY = 
+
     @GET("movie/popular")
-    @Headers("Authorization: Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiJkYzhmNjE2NzE4ZDY0ZjY2NTkyYTc5NzQzNTMxMTdlYyIsIm5iZiI6MTcyMjQ2MjkxNi42NjIzMTMsInN1YiI6IjY2YTc4ZTE3NWI3ZTZlMWQ2ODExZmQ0NyIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.BGMAW4UrNYpBisDHyVxHgzs1YnTAMv2zXWAZ4zhnSks")
-    suspend fun getMovies(@Query("page") page: Int): Movies
+    suspend fun getMovies(
+        @Header("Authorization") authorization: String = "Bearer $API_KEY",
+        @Query("page") page: Int
+    ): Movies
 
     @GET("movie/{movie_id}")
-    @Headers("Authorization: Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiJkYzhmNjE2NzE4ZDY0ZjY2NTkyYTc5NzQzNTMxMTdlYyIsIm5iZiI6MTcyMjQ2MjkxNi42NjIzMTMsInN1YiI6IjY2YTc4ZTE3NWI3ZTZlMWQ2ODExZmQ0NyIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.BGMAW4UrNYpBisDHyVxHgzs1YnTAMv2zXWAZ4zhnSks")
-    suspend fun getMovieDetail(@Path("movie_id") movieId: Int, @Query("language") language: String = "en-US"): MovieDetail
+    suspend fun getMovieDetail(
+        @Header("Authorization") authorization: String = "Bearer $API_KEY",
+        @Path("movie_id") movieId: Int,
+        @Query("language") language: String = "en-US"
+    ): MovieDetail
 
     @GET("discover/movie")
-    @Headers("Authorization: Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiJkYzhmNjE2NzE4ZDY0ZjY2NTkyYTc5NzQzNTMxMTdlYyIsIm5iZiI6MTcyMjQ2MjkxNi42NjIzMTMsInN1YiI6IjY2YTc4ZTE3NWI3ZTZlMWQ2ODExZmQ0NyIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.BGMAW4UrNYpBisDHyVxHgzs1YnTAMv2zXWAZ4zhnSks")
     suspend fun getMoviesByGenre(
-        @Query("api_key") apiKey: String,
+        @Header("Authorization") authorization: String = "Bearer $API_KEY",
         @Query("with_genres") genreId: Int,
         @Query("sort_by") sortBy: String = "popularity.desc"
     ): MovieListResponse
 
     @GET("search/movie")
-    @Headers("Authorization: Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiJkYzhmNjE2NzE4ZDY0ZjY2NTkyYTc5NzQzNTMxMTdlYyIsIm5iZiI6MTcyMjQ2MjkxNi42NjIzMTMsInN1YiI6IjY2YTc4ZTE3NWI3ZTZlMWQ2ODExZmQ0NyIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.BGMAW4UrNYpBisDHyVxHgzs1YnTAMv2zXWAZ4zhnSks")
     suspend fun searchMovies(
+        @Header("Authorization") authorization: String = "Bearer $API_KEY",
         @Query("query") query: String,
         @Query("page") page: Int
     ): Response<MovieResponse>
 
     @GET("discover/movie")
-    @Headers("Authorization: Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiJkYzhmNjE2NzE4ZDY0ZjY2NTkyYTc5NzQzNTMxMTdlYyIsIm5iZiI6MTcyMjQ2MjkxNi42NjIzMTMsInN1YiI6IjY2YTc4ZTE3NWI3ZTZlMWQ2ODExZmQ0NyIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.BGMAW4UrNYpBisDHyVxHgzs1YnTAMv2zXWAZ4zhnSks")
     suspend fun getMoviesByCategory(
+        @Header("Authorization") authorization: String = "Bearer $API_KEY",
         @Query("with_genres") genreId: Int
-    ): Response<MovieResponse>
-
-    @GET("find/{imdb_id}")
-    @Headers("Authorization: Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiJkYzhmNjE2NzE4ZDY0ZjY2NTkyYTc5NzQzNTMxMTdlYyIsIm5iZiI6MTcyMjQ2MjkxNi42NjIzMTMsInN1YiI6IjY2YTc4ZTE3NWI3ZTZlMWQ2ODExZmQ0NyIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.BGMAW4UrNYpBisDHyVxHgzs1YnTAMv2zXWAZ4zhnSks")
-    suspend fun getMovieByImdbId(
-        @Path("imdb_id") imdbId: String,
-        @Query("external_source") externalSource: String = "imdb_id"
     ): Response<MovieResponse>
 }
